e2180e8c14ef738fc8e54c1993e5b540d901cb46
Benjamin Bergman <ben@benbergman.ca>
2013-01-31 21:26:28 -0600

    Add validations

--- app/models/spree/recipient.rb
+++ app/models/spree/recipient.rb
@@ -1,5 +1,8 @@
 module Spree
   class Recipient < ActiveRecord::Base
     attr_accessible :name, :description, :pay_out_note
+
+	validates :name, :uniqueness => true, :presence => true
+	validates :description, :presence => true
   end
 end
